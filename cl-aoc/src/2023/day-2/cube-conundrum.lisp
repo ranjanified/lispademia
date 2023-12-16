9adca9e4c6d2275c5d4561d4eff6321d3e2918f1
Nalin Ranjan <nranjan@ivycomptech.com>
2023-12-16 21:07:18 +0530

    cl-aoc: Day 2, part 2 completed.

--- cl-aoc/src/2023/day-2/cube-conundrum.lisp
+++ cl-aoc/src/2023/day-2/cube-conundrum.lisp
@@ -1,5 +1,7 @@
 (in-package :cl-aoc)
+
 (export 'cube-conundrum)
+(export 'sum-powers)
 ;; (export 'split-game-entry)
 ;; (export 'game-id)
 ;; (export 'read-color)
@@ -10,69 +12,81 @@
 ;; (export 'possible-games)
 ;; (export 'sum-game-ids)
 
-
 (defun cube-conundrum (color-config game-strings)
-  (labels ((sum-game-ids (color-config game-strings)
-	     (apply #'+ (possible-games color-config game-strings)))
-	   
-	   (possible-games (color-config game-strings)
-	     (remove-if #'null
-			(mapcar #'first
-				(remove-if (lambda (g-entry) (not (possible-game-p color-config g-entry)))
-					   (mapcar (lambda (game-string) (make-game-entries game-string)) game-strings)
-					   :key #'rest))))
-	   
-	   (possible-game-p (color-config game-color-config)
-	     ;; game-color-config is a list of list
-	     (and (remove nil game-color-config)
-		  (not (remove nil (mapcar (lambda (config game-colors)
-					     (and (member config game-colors :test (lambda (color g-color) (> g-color color))) t))
-					   color-config game-color-config)))))
-	   
-	   (make-game-entries (game-entry-string)
-	     (let* ((game-value-id "game")
-		    (red-color-id "red")
-		    (green-color-id "green")
-		    (blue-color-id "blue")
-		    (game-entries (split-game-entry game-entry-string))
-		    (red-color (read-color red-color-id (read-entries red-color-id game-entries)))
-		    (green-color (read-color green-color-id (read-entries green-color-id game-entries)))
-		    (blue-color (read-color blue-color-id (read-entries blue-color-id game-entries)))
-		    (game-value (game-id game-value-id (read-entries game-value-id game-entries))))
-	       (cond
-		 ((null game-entry-string) nil)
-		 ((string= (string-trim " " game-entry-string) "") nil)
-		 (t (list game-value red-color green-color blue-color)))))	   
-	   
-	   (split-game-entry (val)
-	     (mapcar (lambda (s)
-		       (uiop:split-string (string-trim " " s) :separator '(#\,)))
-		     (uiop:split-string val :separator '(#\: #\;))))
+  (sum-game-ids color-config game-strings))
 
-	   (read-entries (value-for values &key (test #'string=))
-	     (remove value-for values
-		     :test-not (lambda (val entry-list)
-				 (member val entry-list
-					 :test (lambda (val-1 entry)
-						 (search (string-downcase val-1) (string-downcase entry) :test test))))))
+(defun sum-powers (game-strings)
+  (apply #'+ 
+	 (mapcar (lambda (game-string) (power-cubes (rest (make-game-entries game-string)))) game-strings)))
 
-	   (game-id (game-value-id game-entries)
-	     (first (read-entry game-value-id game-entries)))
-	   
-	   (read-color (color game-entries)
-	     (read-entry color game-entries))
-	   
-	   (read-entry (entry-for entries)
-	     (let ((found-entries (mapcar (lambda (entry-list)
-					    (first (member entry-for entry-list
-							   :test (lambda (val-1 entry)
-								   (search (string-downcase val-1) (string-downcase entry))))))
-					  entries)))
-	       (cond
-		 ((null found-entries) (list))
-		 (t (mapcar (lambda (found)
-			      (parse-integer (coerce (remove-if #'null
-								(coerce found 'list)
-								:key #'digit-char-p) 'string)))
-			    found-entries))))))
-    (sum-game-ids color-config game-strings)))
+(defun power-cubes (game-color-config)
+  (cond
+    ((null game-color-config) 0)
+    (t (apply #'* (possible-min-combinations game-color-config)))))
+
+(defun possible-min-combinations (game-color-config)
+  (mapcar (lambda (y) (apply #'max y)) game-color-config))
+
+(defun sum-game-ids (color-config game-strings)
+  (apply #'+ (possible-games color-config game-strings)))
+
+(defun possible-games (color-config game-strings)
+  (remove-if #'null
+	     (mapcar #'first
+		     (remove-if (lambda (g-entry) (not (possible-game-p color-config g-entry)))
+				(mapcar (lambda (game-string) (make-game-entries game-string)) game-strings)
+				:key #'rest))))
+
+(defun possible-game-p (color-config game-color-config)
+  ;; game-color-config is a list of list
+  (and (remove nil game-color-config)
+       (not (remove nil (mapcar (lambda (config game-colors)
+				  (and (member config game-colors :test (lambda (color g-color) (> g-color color))) t))
+				color-config game-color-config)))))
+
+(defun make-game-entries (game-entry-string)
+  (let* ((game-value-id "game")
+	 (red-color-id "red")
+	 (green-color-id "green")
+	 (blue-color-id "blue")
+	 (game-entries (split-game-entry game-entry-string))
+	 (red-color (read-color red-color-id (read-entries red-color-id game-entries)))
+	 (green-color (read-color green-color-id (read-entries green-color-id game-entries)))
+	 (blue-color (read-color blue-color-id (read-entries blue-color-id game-entries)))
+	 (game-value (game-id game-value-id (read-entries game-value-id game-entries))))
+    (cond
+      ((null game-entry-string) nil)
+      ((string= (string-trim " " game-entry-string) "") nil)
+      (t (list game-value red-color green-color blue-color)))))	   
+
+(defun split-game-entry (val)
+  (mapcar (lambda (s)
+	    (uiop:split-string (string-trim " " s) :separator '(#\,)))
+	  (uiop:split-string val :separator '(#\: #\;))))
+
+(defun read-entries (value-for values &key (test #'string=))
+  (remove value-for values
+	  :test-not (lambda (val entry-list)
+		      (member val entry-list
+			      :test (lambda (val-1 entry)
+				      (search (string-downcase val-1) (string-downcase entry) :test test))))))
+
+(defun game-id (game-value-id game-entries)
+  (first (read-entry game-value-id game-entries)))
+
+(defun read-color (color game-entries)
+  (read-entry color game-entries))
+
+(defun read-entry (entry-for entries)
+  (let ((found-entries (mapcar (lambda (entry-list)
+				 (first (member entry-for entry-list
+						:test (lambda (val-1 entry)
+							(search (string-downcase val-1) (string-downcase entry))))))
+			       entries)))
+    (cond
+      ((null found-entries) (list))
+      (t (mapcar (lambda (found)
+		   (parse-integer (coerce (remove-if #'null
+						     (coerce found 'list)
+						     :key #'digit-char-p) 'string)))
+		 found-entries)))))
--- cl-aoc/tests/2023/day-2/cube-conundrum.lisp
+++ cl-aoc/tests/2023/day-2/cube-conundrum.lisp
@@ -8,11 +8,16 @@
 			       "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
 			       "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
 			       "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
-			       "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")))
+				"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
+	(game-strings-from-input-file (uiop:read-file-lines
+				       (merge-pathnames (asdf:system-relative-pathname "cl-aoc" "tests/2023/day-2/")
+							#P"input.txt"))))
     (is-true (zerop (cube-conundrum color-config '())))
     (is-true (zerop (cube-conundrum color-config '(""))))
     (is-true (= 8 (cube-conundrum color-config example-game-strings)))
-    (is (= 2512 (cube-conundrum color-config
-				 (uiop:read-file-lines
-				  (merge-pathnames (asdf:system-relative-pathname "cl-aoc" "tests/2023/day-2/")
-						   #P"input.txt")))))))
+    (is (= 2512 (cube-conundrum color-config game-strings-from-input-file)))
+
+    (is-true (zerop (sum-powers '())))
+    (is-true (zerop (sum-powers '(""))))
+    (is-true (= 2286 (sum-powers example-game-strings)))
+    (is-true (= 67335 (sum-powers game-strings-from-input-file)))))
