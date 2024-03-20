c8a50d236f690bfed43a661f54313b130e184bd0
Nalin Ranjan <nranjan@ivycomptech.com>
2024-03-20 12:40:51 +0530

    cl-dsa: refactor weather-report
    1. unused variable collect-average-of removed,
    2. commented-out debugging code removed
    3. Few code-block comments added
    4. lambda list of average-of-averages function changed to drop  rest
    parameters
    5. calls to average-of-averages adjusted to its new lambda list

--- cl-dsa/src/weather-report.lisp
+++ cl-dsa/src/weather-report.lisp
@@ -2,8 +2,8 @@
 
 (defun weather-report (readings)
   ;; (declare (type (simple-array (simple-array (or (integer -90 -1) (integer 1 90) (single-float)) 2) *) readings))
-  (prog* ((average-of-averages (lambda (&rest averages)
-				(loop :for s :in averages
+  (prog* ((average-of-averages (lambda (averages)
+				(loop :for s :across averages
 				      :when s
 					:sum s :into sum-sum :and :count s :into count-count
 				      :finally (return (if (zerop count-count) nil (float (/ sum-sum count-count)))))))
@@ -20,31 +20,28 @@
 			     (loop :repeat 90
 				   :collect (make-array '(90) :adjustable t :fill-pointer 0 :initial-element (list)))))
 	 (average-readings-table (make-array '(90 3) :initial-contents (loop :repeat 90 :collect (vector nil nil nil))))
-	 (collect-averages-of (lambda (readings-table reading-index)
-				(loop :for index :from 0 :below (array-dimension readings-table 0)
-				      ;; :do (format t "~a ~a" index (aref readings-table index 0))
-				      :collect (aref readings-table index reading-index))))
 	 (calculate-warmer (lambda (readings-table)
 			     (loop
 			       :for index :from 0 :below (array-dimension readings-table 0)
 			       :when (and (aref readings-table index 0)
 					  (aref readings-table index 1))
 				 :collect (aref readings-table index 0) :into n-averages
-				 ;; :and :count (aref readings-table index 0) :into n-counts
 				 :and :collect (aref readings-table index 1) :into s-averages
-			       :finally (return (let ((n-avg (apply average-of-averages n-averages))
-						      (s-avg (apply average-of-averages s-averages)))
+			       :finally (return (let ((n-avg (funcall average-of-averages (coerce n-averages 'simple-vector)))
+						      (s-avg (funcall average-of-averages (coerce s-averages 'simple-vector))))
 						  (vector n-avg
 							  s-avg
 							  (if (> n-avg s-avg)
 							      "northern hemisphere"
 							      "southern hemisphere"))))))))
+     ;; filter readings in northern and southern buckets
      (loop :for reading :across readings
 	   :do (vector-push (svref reading 1)
 			    (aref (if (plusp (svref reading 0))
 				      northern-readings
 				      southern-readings)
 				  (1- (abs (svref reading 0))))))
+     ;; Calculate of averages of each latitude in their respective buckets
      (loop
        :for index := 1 :then (1+ index)
        :for northern-reading :across northern-readings
@@ -54,12 +51,13 @@
        :unless (zerop (length southern-reading))
 	 :do (setf (aref average-readings-table (1- index) 1) (funcall average-readings southern-reading))
        :do (setf (aref average-readings-table (1- index) 2) (funcall average-of-averages
-								     (aref average-readings-table (1- index) 0)
-								     (aref average-readings-table (1- index) 1))))
+								     (vector (aref average-readings-table (1- index) 0)
+									     (aref average-readings-table (1- index) 1)))))
+     ;; Print the table and warmer hemisphere info
      (loop
+       :initially (format t "~:(~2@a~)~t~:(~16@a~)~t~:(~16@a~)~t~:(~16@a~)~&"
+			  "latitude" "northern hemisphere" "southern hemisphere" "average-readings")
        :for index :from 0 :below (array-dimension average-readings-table 0)
-	 :initially (format t "~:(~2@a~)~t~:(~16@a~)~t~:(~16@a~)~t~:(~16@a~)~&"
-			    "latitude" "northern hemisphere" "southern hemisphere" "average-readings")
        :do (format t "~2@a.~t~:@(~18@a~)~:@(~18@a~)~:@(~18@a~)~&"
 		   (1+ index)
 		   (or (aref average-readings-table index 0) "no data")
