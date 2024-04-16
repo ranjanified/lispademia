da70c1266f90be8b161e9e6163d6ffe6585be6b0
Nalin Ranjan <nranjan@ivycomptech.com>
2024-04-16 11:28:08 +0530

    cl-sbcl-ffi: modern-c, chapter 5
    1. added example call docs in case of error while worded-number
    2. added example call docs while count-digits

--- cl-sbcl-ffi/src/modern-c/chapter5.lisp
+++ cl-sbcl-ffi/src/modern-c/chapter5.lisp
@@ -7,6 +7,13 @@
 ;;;   (with-alien ((number-string c-string worded-number))
 ;;;     (format t "Number as Word: ~a~%" number-string))
 ;;;   (free-alien worded-number))
+;;; or when error,
+;;; (with-alien ((worded-number (* char) (cl-sbcl-ffi:worded-number 1)))
+;;;   (with-alien ((number-string c-string worded-number))
+;;;     (format t "Number as Word: ~a, ~a~%" number-string (get-errno)))
+;;;   (free-alien worded-number))
 (define-alien-routine "worded_number" (* char) (two-digit-number unsigned-short))
 
+;;; example call
+;;; (cl-sbcl-ffi:count-digits 999)
 (define-alien-routine "count_digits" unsigned-short (number unsigned-long))
