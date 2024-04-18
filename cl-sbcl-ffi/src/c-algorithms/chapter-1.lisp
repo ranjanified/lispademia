106d0065d809b076ec0f416c455182c9968fb56a
Nalin Ranjan <nranjan@ivycomptech.com>
2024-04-18 16:51:25 +0530

    cl-sbcl-ffi: c-algorithms, chapter 1
    1. removed call to free-alien from example call of reduce-fraction

--- cl-sbcl-ffi/src/c-algorithms/chapter-1.lisp
+++ cl-sbcl-ffi/src/c-algorithms/chapter-1.lisp
@@ -3,13 +3,13 @@
 (define-alien-routine "euclid_gcd" unsigned-int (num1 unsigned-long) (num2 unsigned-long))
 
 ;;; example call:
+;;; the binding fraction is a local alien. Lisp should be doing GC for it
 ;;; (with-alien ((fraction cl-sbcl-ffi:fraction))
 ;;;   (setf (slot fraction 'numerator) 7)
 ;;;   (setf (slot fraction 'denominator) 5)
 ;;;   (cl-sbcl-ffi:reduce-fraction (addr fraction))
 ;;;   (format t "Numerator: ~a~%Denominator: ~a~%" 
 ;;; 	  (slot fraction 'numerator) 
-;;; 	  (slot fraction 'denominator))
-;;;   (free-alien fraction)))
+;;; 	  (slot fraction 'denominator)))
 (define-alien-type fraction (struct fraction (numerator int) (denominator int)))
 (define-alien-routine "reduce_fraction" void (curr-fraction (* fraction)))
